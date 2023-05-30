4c1a14b60361f7235774341c152dd5c078b2e571
Kay <info@beckmann-md.de>
2023-05-30 21:33:23 +0200

    Slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'kaybeckmann';
+export class AppComponent implements OnInit {
+  images = [`1.jpg`, `2.jpg`, `3.jpg`];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(): void {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 20);
+    }, 5000);
+  }
 }
